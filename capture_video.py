e786244b01158fe1ffa5400151c93378d9865f31
Weliton Sousa <welitonubuntu@gmail.com>
2020-07-11 01:34:13 -0300

    rectangle in face detected

--- capture_video.py
+++ capture_video.py
@@ -1,9 +1,18 @@
 import cv2
+
 cam = cv2.VideoCapture(0)
+classificator = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 while (True) :
     conected, image = cam.read()
+    imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    facedetected = classificator.detectMultiScale(imageGray, scaleFactor = 1.5, minSize = (100, 100))
+
+    for (x, y, width, heigth) in facedetected:
+        cv2.rectangle(image, (x, y), (x + width, y + heigth), (0,0,255), 2)
+
     cv2.imshow("face", image)
     cv2.waitKey(1)
+
 cam.release()
 cv2.destroyAllWindows()
